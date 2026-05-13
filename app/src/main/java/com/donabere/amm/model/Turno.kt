a911e03f164fd9213b49332516358e252dc5d1de
javierUPAO <jaguilarb3@upao.edu.pe>
2026-05-13 11:47:26 -0500

    feat: Se añadio el repositorio para turno

--- app/src/main/java/com/donabere/amm/model/Turno.kt
+++ app/src/main/java/com/donabere/amm/model/Turno.kt
@@ -6,9 +6,9 @@ import com.google.firebase.Timestamp
 data class Turno(
     val id: String = "",
     val mozoId: String = "",
-    val inicio: Timestamp = Timestamp.now(),
+    val inicio: Timestamp? = Timestamp.now(),
     val fin: Timestamp? = null,
-    val estado: EstadoTurno = EstadoTurno.ABIERTO,
+    val estado: EstadoTurno = EstadoTurno.CERRADO,
     val totalMesas: Int = 0,
     val totalPedidos: Int = 0,
     val totalVendido: Double = 0.0
